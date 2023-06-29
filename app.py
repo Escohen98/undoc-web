adf216333bb2759b70edd1c4519e6fe35b5848db
Eric <you@example.com>
2023-06-29 14:20:52 -0400

    Works like a charm :")

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
 from distutils.log import debug
 from fileinput import filename
 from convert import converter
+import os
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,19 +16,27 @@ def uploaded():
     if request.method == 'POST':
         print("Post request")
         f=request.files['file-txt']
+        filepath = os.path.join('static', 'downloads', f.filename)
         print("saving...")
-        f.save(f.filename)
+        f.save(filepath)
         print("saved.")
-        c = converter().run(f.filename)  # Get the path of the converted text file
-        print("Working...")
+        c = converter().run(filepath)  # Get the path of the converted text file
         lines = []  # Initialize lines
         with open(c, 'r') as file:  # Open the converted text file
             lines = [line.rstrip() for line in file if line.strip()]  # Read lines from the converted text file. Strips new line char
-        converter().delFile(c)  # Delete the converted text file
-        converter().delFile(f.filename) # Deleted the original doc(x) file.
-        response = render_template('./uploaded.html', file=lines)
+        #converter().delFile(c)  # Delete the converted text file
+        #converter().delFile(f.filename) # Deleted the original doc(x) file.
+        print("filename: " + c)
+        filename = c.split("\\")[len(c.split("\\"))-1]
+        print("ne filename: " + filename)
+        response = render_template('./uploaded.html', file=lines, filename=filename)
     return response
 
+#File download handler
+@app.route('/download/<filename>')
+def download(filename):
+    print(f'Attempting to send: {filename}')
+    return send_from_directory(directory='static/downloads/', path=filename, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1337, debug=True)
